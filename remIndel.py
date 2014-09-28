e557abb6754efe546d6d3be2e8c164b0d6b7623c
Ambuj Kumar <sendambuj@gmail.com>
2014-09-28 01:24:01 -0400

    Update remIndel.py

--- remIndel.py
+++ remIndel.py
@@ -19,12 +19,12 @@
 #                                                                                                              #
 ################################################################################################################
 
-
 import argparse
 import textwrap
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
+from Bio.Align import MultipleSequenceAlignment as msa
 
 parser = argparse.ArgumentParser(prog='Remove-Indel',
                                  version= '1.0',
@@ -41,8 +41,15 @@ parser.add_argument('-i', type=str, required = True, help='Enter input alignment
 parser.add_argument('-o', type=str, required = True, help='Enter output alignment filename')
 parser.add_argument('-itype', type=str, required = True, choices=['fasta', 'nexus', 'phylip', 'phylip-interleived', 'phylip-relaxed'], help='Enter input alignment file format')
 parser.add_argument('-otype', type=str, required = True, choices=['fasta', 'nexus', 'phylip', 'phylip-interleived', 'phylip-relaxed'], help='Enter output alignment file format')
+parser.add_argument('-c', action='store_true', default=False, help='Use if you want species centric alignment correction')
+parser.add_argument('-name', type=str, default = None, required = True, help='Enter species name')
+
 
 args = parser.parse_args()
+
+if args.c == True and args.name == None:
+    print("-name required in -c mode\n")
+
 
 handle = open(args.i, 'rU')
 records = list(SeqIO.parse(handle, args.itype))
@@ -50,8 +57,44 @@ records = list(SeqIO.parse(handle, args.itype))
 def split(str, num):
     return [ str[start:start+num] for start in range(0, len(str), num) ]
 
+def group(L):
+    first = last = L[0]
+    for n in L[1:]:
+        if n - 1 == last:
+            last = n
+        else:
+            yield first, last
+            first = last = n
+    yield first, last
+
+
+def checkSpecies(records):
+    posList = list()
+    for rec in records():
+        if rec.id == args.name:
+            dataSplit = split(rec.seq)
+            for i, data in enumerate(dataSplit):
+                if '-' in data:
+                    posList.append(i)
+
+    groupData = [x for x in group(posList)]
+    msaObject = msa(records)
+    for val in groupData:
+        if dataSplit[val[0]].count('-') != 3 and dataSplit[val[1]].count('-') != 3:
+            msaObject = msaObject[0:(val[0]-1)*3] + msaObject[(val[1]*3)+1:len(msaObject)]
+
+    for i, rec in enumerate(records):
+        records[i].seq = Seq(msaObject[rec.id], generic_dna)
+
+    return records
+
+
 
 def main():
+    
+    if args.c == True:
+        records = checkSpecies(records)
+    
     for i, rec in enumerate(records):
         newSeq = Seq("", generic_dna)
         seqData = split(rec.seq, 3)
